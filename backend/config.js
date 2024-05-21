50dd22834fe40072e09bfb3f53e84d9e79b4de14
IslemKammoun <kammounislem@gmail.com>
2024-05-21 01:19:50 +0100

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:BExsIrDBaY0gH77O@bookstore.hvzck3m.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
